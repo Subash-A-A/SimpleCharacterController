037a7e08a6df078089e2789d420e2acb86699868
Subash <it.subashaa@gmail.com>
2021-12-10 09:56:38 +0530

    arrowKeys

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const charSprite = document.querySelector(".Character_spritesheet");
 let lastDirection;
 
-document.addEventListener("keypress", function (event) {
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
   lastDirection = event.key;
-  if (event.key == "w") {
+  if (event.key == "w" || event.key == "ArrowUp") {
     charSprite.classList.remove("face-down");
     charSprite.classList.remove("face-left");
     charSprite.classList.remove("face-right");
     charSprite.classList.add("face-up");
-  } else if (event.key == "s") {
+  } else if (event.key == "s" || event.key == "ArrowDown") {
     charSprite.classList.add("face-down");
     charSprite.classList.remove("face-left");
     charSprite.classList.remove("face-right");
     charSprite.classList.remove("face-up");
-  } else if (event.key == "a") {
+  } else if (event.key == "a" || event.key == "ArrowLeft") {
     charSprite.classList.remove("face-down");
     charSprite.classList.add("face-left");
     charSprite.classList.remove("face-right");
     charSprite.classList.remove("face-up");
-  } else if (event.key == "d") {
+  } else if (event.key == "d" || event.key == "ArrowRight") {
     charSprite.classList.remove("face-down");
     charSprite.classList.remove("face-left");
     charSprite.classList.add("face-right");
